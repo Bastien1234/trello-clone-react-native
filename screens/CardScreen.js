fc5125228a8d07da068e4d7bc992215ce2925836
unknown <bastien.clementi@gmail.com>
2022-05-24 14:40:54 +0200

    doing things

--- screens/CardScreen.js
+++ screens/CardScreen.js
@@ -31,6 +31,8 @@ const WorkspaceScreen = ({ navigation }) => {
 
     const [database, setDatabase] = useState(card);
     const [writingText, setWritingText] = useState(false);
+    const [addingChecklist, setAddingChecklist] = useState(false);
+    const [newChecklistElementValue, setNewChecklistElementValue] = useState("");
 
     let [textDB, setTextDB] = useState("sdfjkhsdkfjhdsk jdfkjh kfdjhkdj hfdkfjhdfkjdhfkjh fdkjhfdkjhfdkjhf");
     const [currentWritingText, setCurrentWritingText] = useState("");
@@ -166,6 +168,52 @@ const WorkspaceScreen = ({ navigation }) => {
 
                 }
 
+                {
+                    addingChecklist === false ?
+
+                    <Pressable
+                        style={{...styles.bottomButton, width: "85%", alignSelf: "center", alignItems: "center"}}
+                        onPress={() => setAddingChecklist(true)}>
+                        <Text>Ajouter element à la checklist</Text>
+                    </Pressable>
+
+                    :
+
+                    <View>
+                        <TextInput 
+                        style={{
+                            height: 60,
+                            width: "100%",
+                            borderWidth: 1,
+                            borderColor: "lightgrey",
+                            paddingLeft: 15
+                        }}
+                        placeholder="Nouvel élément"
+                        value={newChecklistElementValue}
+                        onChangeText={t => setNewChecklistElementValue(t)}/>
+
+                        <View style={{
+                            flexDirection: "row",
+                            height: 50,
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}>
+                            <Pressable 
+                                style={styles.bottomButton}
+                                onPress={() => {
+                                    setAddingChecklist(false)
+                                }}>
+                                <Text>Annuler</Text>
+                            </Pressable>
+
+                            <Pressable style={styles.bottomButton}>
+                                <Text>Ajouter</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                    
+                }
+
                 
 
             </View>
@@ -231,5 +279,14 @@ const styles = StyleSheet.create({
         height: 50,
         alignItems: "center",
 
+    },
+
+    bottomButton: {
+        marginTop: 15,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 5,
+        backgroundColor: "lightgrey",
+        padding: 10
     }
 })
